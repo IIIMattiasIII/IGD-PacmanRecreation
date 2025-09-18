3bbf0db3ce94aba9ffc8cf888efaacff6201224a
Mattias Przyrembel <88617353+IIIMattiasIII@users.noreply.github.com>
2025-09-19 01:26:34 +1000

    Add dynamic tile rotations
    Plus generally tidied level generator script

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -14,6 +14,8 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] private float cameraPadding = 1;
     private Tilemap wallsMap;
     private Tilemap pelletsMap;
+    private static readonly int[] walls = { 1, 2, 3, 4, 7, 8 };
+    private static readonly int[] nonwalls = { 0, 5, 6 };
 
     void Awake()
     {
@@ -74,9 +76,12 @@ public class LevelGenerator : MonoBehaviour
         This could be addressed properly, but it would involve assuming a specific aspect ratio of the output display. Irrespective
         of this, it seemed beyond the scope of this task. If I'm presuming wrong here, feel free to note that in my marking and I'll 
         include this functionality in AT4. */
+        // Please also note that there is a cameraPadding variable which also affects the overall size
         float max = Math.Max(levelMap.GetLength(0), levelMap.GetLength(1));
         Camera.main.orthographicSize = (float)Math.Ceiling(max / 2) + cameraPadding;
     }
+
+    /* ========== Tile Placing ========== */
 
     Tilemap CreateTilemap(string objName)
     {
@@ -87,14 +92,9 @@ public class LevelGenerator : MonoBehaviour
         return tilemap;
     }
 
-    void PlaceTile(Vector3Int pos, int tile)
+    bool PlaceTile(Vector3Int pos, int tile)
     {
-        if (tile < 0 || tile >= tiles.Length)
-        {
-            Debug.LogWarning($"Invalid tile index ({tile}) in level csv.");
-            return;
-        }
-
+        if (tile < 0 || tile >= tiles.Length) { return false; }
         if (tile == 5 || tile == 6)
         {
             pelletsMap.SetTile(pos, tiles[tile]);
@@ -103,6 +103,7 @@ public class LevelGenerator : MonoBehaviour
         {
             wallsMap.SetTile(pos, tiles[tile]);
         }
+        return true;
     }
 
     void GenerateTilemap()
@@ -114,14 +115,168 @@ public class LevelGenerator : MonoBehaviour
         int height = levelMap.GetLength(1);
 
         int offsetX = -Mathf.FloorToInt(width / 2);
-        int offsetY = -Mathf.CeilToInt((height+1) / 2);
+        int offsetY = -Mathf.CeilToInt((height + 1) / 2);
 
         for (int y = 0; y < height; y++)
         {
             for (int x = 0; x < width; x++)
             {
-                PlaceTile(new Vector3Int(x + offsetX, y + offsetY, 0), levelMap[x, y]);
+                Vector3Int pos = new(x + offsetX, y + offsetY, 0);
+                int tile = levelMap[x, y];
+                if (!PlaceTile(pos, tile))
+                {
+                    Debug.LogWarning($"Invalid tile index ({tile}) in level csv.");
+                    continue;
+                }
+                if (GetTileRotation(x, y, tile, width, height, out float rotation))
+                {
+                wallsMap.SetTransformMatrix(pos, Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(0, 0, rotation), Vector3.one));
+                }
             }
         }
+    }
+
+    /* ========== Tile Rotations ========== */
+
+    int GetNeighbourValue(int currentX, int currentY, int dx, int dy)
+    {
+        int checkX = currentX + dx;
+        int checkY = currentY + dy;
+        if (checkX >= 0 && checkX < levelMap.GetLength(0) && checkY >= 0 && checkY < levelMap.GetLength(1))
+        {
+            return levelMap[checkX, checkY];
+        }
+        return -1;
+    }
+
+    bool CanConnect(int neighbourType)
+    {
+        // Note: I had considered checking with a 'fromType' so outside doesn't connect with inside, and similar for each side of junctions, 
+        // but decided that wasn't necessary as no (proper) map design should have these wall types touching improperly - there should always be 
+        // a walking gap or similar between them.
+        if (neighbourType == -1 || neighbourType == 0 || neighbourType == 5 || neighbourType == 6)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    float GetTJuncRotation(int x, int y, bool up, bool down, bool left, bool right)
+    {
+        if (left && right && down && !up) return 0;
+        else if (up && down && left && !right) return -90;
+        else if (left && right && up && !down) return -180;
+        else if (up && down && right && !left) return -270;
+
+        Debug.LogWarning($"T Junction at position ({x}, {y}): calculation failed to determine an appropriate rotation");
+        return 0;
+    }
+
+    float GetCornerRotation(int tile, int x, int y, bool up, bool down, bool left, bool right)
+    {
+        // Simple Corners - use nearby walls/corners for rotation
+
+        if (right && down && !up && !left) return 0;
+        if (left && down && !up && !right) return -90;
+        if (left && up && !down && !right) return -180;
+        if (right && up && !down && !left) return -270;
+
+        if (tile == 1) // Outside corners shouldn't need complex corner checks
+        {
+            Debug.LogWarning($"Outside corner at position ({x}, {y}): calculation failed to determine an appropriate rotation");
+            return 0;
+        }
+
+        // Complex Corners (extra nearby walls) - use blank space nearby for position
+
+        bool diagUpLeft = !CanConnect(GetNeighbourValue(x, y, -1, 1));
+        bool diagUpRight = !CanConnect(GetNeighbourValue(x, y, 1, 1));
+        bool diagDownLeft = !CanConnect(GetNeighbourValue(x, y, -1, -1));
+        bool diagDownRight = !CanConnect(GetNeighbourValue(x, y, 1, -1));
+
+        if (diagDownRight && left && up) return 0;
+        if (diagDownLeft && right && up) return -90;
+        if (diagUpLeft && right && down) return -180;
+        if (diagUpRight && left && down) return -270;
+
+        Debug.LogWarning($"Inside corner at position ({x}, {y}): calculation failed to determine an appropriate rotation");
+        return 0;
+    }
+
+    float GetWallRotation(int tile, int x, int y, bool up, bool down, bool left, bool right, int neighbourUp, int neighbourDown, int neighbourLeft, int neighbourRight)
+    {
+        bool emptyUp = nonwalls.Contains(neighbourUp);
+        bool emptyDown = nonwalls.Contains(neighbourDown);
+        bool emptyLeft = nonwalls.Contains(neighbourLeft);
+        bool emptyRight = nonwalls.Contains(neighbourRight);
+
+        if (emptyLeft || emptyRight)
+        {
+            if (up || down || (emptyLeft && emptyRight))
+            {
+                return 90;
+            }
+        }
+        if (emptyUp || emptyDown)
+        {
+            if (left || right || (emptyUp && emptyDown))
+            {
+                return 0;
+            }
+        }
+
+        // Fallback case - prioritise having more connections
+
+        if (left && right && (!up || !down)) return 0;
+        if (up && down && (!left || !right)) return 90;
+
+        Debug.LogWarning($"Wall at position ({x}, {y}): calculation failed to determine an appropriate rotation");
+        return 0;
+    }
+
+    bool GetTileRotation(int x, int y, int tile, int width, int height, out float rotation)
+    {
+        rotation = 0;
+        if (walls.Contains(tile))
+        {
+            int neighbourUp = GetNeighbourValue(x, y, 0, 1);
+            int neighbourDown = GetNeighbourValue(x, y, 0, -1);
+            int neighbourLeft = GetNeighbourValue(x, y, -1, 0);
+            int neighbourRight = GetNeighbourValue(x, y, 1, 0);
+
+            bool up = CanConnect(neighbourUp);
+            bool down = CanConnect(neighbourDown);
+            bool left = CanConnect(neighbourLeft);
+            bool right = CanConnect(neighbourRight);
+
+            switch (tile)
+            {
+                case 7:
+                    rotation = GetTJuncRotation(x, y, up, down, left, right);
+                    break;
+                case 1:
+                case 3:
+                    rotation = GetCornerRotation(tile, x, y, up, down, left, right);
+                    break;
+                case 2:
+                case 4:
+                case 8:
+                    rotation = GetWallRotation(tile, x, y, up, down, left, right, neighbourUp, neighbourDown, neighbourLeft, neighbourRight);
+                    if (tile == 8)
+                    {
+                        // Note: My gate sprite is off-center and presumes it will connect with another based on the given mirroring of the level.
+                        // If test cases have the gate on the vertical sides, there would only be one gate, and the sprite will look odd.
+                        // This could obviously be easily handled by having another tile with a non-connecting sprite, but given we're limited to 8, 
+                        // it will just have to look funny for now.
+                        if (rotation == 0 && x >= width / 2f)
+                            rotation -= 180;
+                        if (rotation == 90 && y >= height / 2f)
+                            rotation -= 180;
+                    }
+                    break;
+            }
+            return true;
+        }
+        return false;
     }
 }
