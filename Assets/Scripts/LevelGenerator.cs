d531aa77805a80b1cfc8ccf9491a71ea5abf9180
Mattias Przyrembel <88617353+IIIMattiasIII@users.noreply.github.com>
2025-09-18 20:22:23 +1000

    Add basic tile placing
    Note: Neighbour orientation relativity not included

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -7,10 +7,13 @@ public class LevelGenerator : MonoBehaviour
 {
     [Tooltip("To the marker: This is the csv for the level file. It must not include trailing commas or blank lines. See the given level layout in the file '/Levels/level01.csv' for an example.")]
     [SerializeField] private TextAsset levelFile;
+    [SerializeField] private TileBase[] tiles;
     private Grid grid;
     private int[,] levelMap;
     [Tooltip("Defines additional padding relative to the level layout for when resizing the camera view area.")]
     [SerializeField] private float cameraPadding = 1;
+    private Tilemap wallsMap;
+    private Tilemap pelletsMap;
 
     void Awake()
     {
@@ -49,8 +52,18 @@ public class LevelGenerator : MonoBehaviour
 
     void Start()
     {
+        if (tiles.Length == 0 || Math.Max(levelMap.GetLength(0), levelMap.GetLength(1)) == 0)
+        {
+            Debug.LogWarning("Level generator is enabled but one or more required fields have not been defined.");
+            return;
+        }
         SetCamera();
-        // GenerateTilemap();
+        for (int i = 0; i < grid.transform.childCount; i++) // Deactivate manually created tilemaps
+        {
+            Transform childTransform = grid.transform.GetChild(i);
+            childTransform.gameObject.SetActive(false);
+        }
+        GenerateTilemap();
     }
 
     void SetCamera()
@@ -62,9 +75,53 @@ public class LevelGenerator : MonoBehaviour
         of this, it seemed beyond the scope of this task. If I'm presuming wrong here, feel free to note that in my marking and I'll 
         include this functionality in AT4. */
         float max = Math.Max(levelMap.GetLength(0), levelMap.GetLength(1));
-        if (max > 0) // if levelmap loaded, adjust camera
+        Camera.main.orthographicSize = (float)Math.Ceiling(max / 2) + cameraPadding;
+    }
+
+    Tilemap CreateTilemap(string objName)
+    {
+        GameObject tilemapGameObject = new GameObject(objName);
+        tilemapGameObject.transform.SetParent(grid.transform);
+        Tilemap tilemap = tilemapGameObject.AddComponent<Tilemap>();
+        tilemapGameObject.AddComponent<TilemapRenderer>();
+        return tilemap;
+    }
+
+    void PlaceTile(Vector3Int pos, int tile)
+    {
+        if (tile < 0 || tile >= tiles.Length)
         {
-            Camera.main.orthographicSize = (float)Math.Ceiling(max / 2) + cameraPadding;
+            Debug.LogWarning($"Invalid tile index ({tile}) in level csv.");
+            return;
+        }
+
+        if (tile == 5 || tile == 6)
+        {
+            pelletsMap.SetTile(pos, tiles[tile]);
+        }
+        else
+        {
+            wallsMap.SetTile(pos, tiles[tile]);
+        }
+    }
+
+    void GenerateTilemap()
+    {
+        wallsMap = CreateTilemap("WallsGenerated");
+        pelletsMap = CreateTilemap("PelletsGenerated");
+
+        int width = levelMap.GetLength(0);
+        int height = levelMap.GetLength(1);
+
+        int offsetX = -Mathf.FloorToInt(width / 2);
+        int offsetY = -Mathf.CeilToInt((height+1) / 2);
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                PlaceTile(new Vector3Int(x + offsetX, y + offsetY, 0), levelMap[x, y]);
+            }
         }
     }
 }
